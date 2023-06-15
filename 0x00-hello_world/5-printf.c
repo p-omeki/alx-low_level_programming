41cd8d405936ce10b15a23a9a3f345bf8fa1151e
p-omeki <partymeki@gmail.com>
2023-06-15 05:22:47 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific string to the standard output
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-    return 0;
+	return (0);
 }
+
