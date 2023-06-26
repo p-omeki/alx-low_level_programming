9c894f572810471b1788b1cefe52cb6ac50e5fbc
p-omeki <partymeki@gmail.com>
2023-06-26 12:07:49 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,14 +6,14 @@
  */
 void puts2(char *str)
 {
-  int i = 0;
+	int i = 0;
 
-  while (*(str + i) != '\0')
-  {
-    _putchar(*(str + i));
-    i += 2;
-  }
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
 
-  _putchar('\n');
+	_putchar('\n');
 }
 
