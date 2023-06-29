be30985814ab918c34e30c67a8abd2026d8cf0ea
p-omeki <partymeki@gmail.com>
2023-06-29 00:25:00 -0700

    Always look up

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *string_toupper(char *str);
 void reverse_array(int *a, int n);
 int _strcmp(const char *s1, const char *s2);
 char *_strncpy(char *dest, const char *src, int n);
