c830a9dc916b2f07d5724ca3f301548990344c4e
p-omeki <partymeki@gmail.com>
2023-07-03 07:44:22 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -10,23 +10,23 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			_putchar(' ');
+		}
+		if (!(i % 10) && i)
+		{
+			_putchar('\n');
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	_putchar('\n');
 }
 
 /**
@@ -36,12 +36,20 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+	char src[10] = "Hello";
+	char dest[10] = "";
+	unsigned int n = 5;
 
-    simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
+	printf("Before memcpy:\n");
+	printf("src: %s\n", src);
+	printf("dest: %s\n", dest);
+
+	_memcpy(dest, src, n);
+
+	printf("After memcpy:\n");
+	printf("src: %s\n", src);
+	printf("dest: %s\n", dest);
+
+	return (0);
 }
+
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _memcpy - Copies n bytes from memory area src to memory area dest.
- * @dest: Pointer to the destination memory area.
- * @src: Pointer to the source memory area.
- * @n: Number of bytes to be copied.
+ * _memcpy - Copies n bytes from memory area src to memory area dest
+ * @dest: Pointer to the destination memory area
+ * @src: Pointer to the source memory area
+ * @n: Number of bytes to be copied
  *
- * Return: A pointer to the destination memory area (dest).
+ * Return: A pointer to the destination memory area (dest)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -16,5 +16,16 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 
 	return dest;
+}
+
+/**
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, return the character written. On error, return -1.
+ */
+int _putchar(char c)
+{
+	return putchar(c);
 }
 
