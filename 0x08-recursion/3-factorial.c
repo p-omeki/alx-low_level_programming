dfe2e26a18f332a2bdfc2a946251e05432a46c69
p-omeki <partymeki@gmail.com>
2023-07-05 00:02:28 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,10 +9,10 @@
 int factorial(int n)
 {
 	if (n < 0)
-		return -1; /* Error: Invalid input */
+		return (-1); /* Error: Invalid input */
 	else if (n == 0)
-		return 1; /* Base case: factorial of 0 is 1 */
+		return (1); /* Base case: factorial of 0 is 1 */
 	else
-		return n * factorial(n - 1); /* Recursive call */
+		return (n * factorial(n - 1)); /* Recursive call */
 }
 
