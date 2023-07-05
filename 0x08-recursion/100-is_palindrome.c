46c333b7edcf212f9a1370c6ccdfba3082f63f1c
p-omeki <partymeki@gmail.com>
2023-07-05 00:57:17 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,28 +1,50 @@
 #include "main.h"
 
+/**
+ * is_palindrome_helper - Checks if a string is a palindrome using recursion.
+ * @s: The string to check.
+ * @start: The starting index.
+ * @end: The ending index.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
 int is_palindrome_helper(char *s, int start, int end);
 
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to check.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
 int is_palindrome(char *s)
 {
 	int end = 0;  /* Declare end variable at the top of the function */
 
 	if (*s == '\0')
-		return 1;  /* Empty string is a palindrome */
+		return (1);  /* Empty string is a palindrome */
 
 	while (s[end] != '\0')
 		end++;
 
-	return is_palindrome_helper(s, 0, end - 1);
+	return (is_palindrome_helper(s, 0, end - 1));
 }
 
+/**
+ * is_palindrome_helper - Checks if a string is a palindrome using recursion.
+ * @s: The string to check.
+ * @start: The starting index.
+ * @end: The ending index.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
 int is_palindrome_helper(char *s, int start, int end)
 {
 	if (start >= end)
-		return 1;  /* Palindrome condition met */
+		return (1);  /* Palindrome condition met */
 
 	if (s[start] != s[end])
-		return 0;  /* Not a palindrome */
+		return (0);  /* Not a palindrome */
 
-	return is_palindrome_helper(s, start + 1, end - 1);
+	return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
