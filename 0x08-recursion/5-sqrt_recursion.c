bb96e221a98ee3729c6b51eed2809d187fbf7dd7
p-omeki <partymeki@gmail.com>
2023-07-05 00:24:05 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,7 +18,7 @@ int calculate_sqrt(int n, int guess);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return (-1);  /* Error: Invalid input */
+		return (-1); /* Error: Invalid input */
 	else
 		return calculate_sqrt(n, 0);
 }
@@ -33,10 +33,10 @@ int _sqrt_recursion(int n)
 int calculate_sqrt(int n, int guess)
 {
 	if (guess * guess == n)
-		return guess;  /* Base case: found the square root */
+		return guess; /* Base case: found the square root */
 	else if (guess * guess > n)
-		return -1;     /* Error: n does not have a natural square root */
+		return -1; /* Error: n does not have a natural square root */
 	else
-		return calculate_sqrt(n, guess + 1);  /* Recursive call with incremented guess */
+		return calculate_sqrt(n, guess + 1); /* Recursive call with incremented guess */
 }
 
