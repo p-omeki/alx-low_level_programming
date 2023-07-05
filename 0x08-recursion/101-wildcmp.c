ebfb3f70af6dc84e98eff71939bd26229e87b4f6
p-omeki <partymeki@gmail.com>
2023-07-05 03:26:47 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,45 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * wildcmp - Compares two strings with wildcard pattern matching.
- * @s1: The first string.
- * @s2: The second string with wildcard pattern.
+ * calculate_sqrt - Calculates the square root of a number using recursion.
+ * @n: The number to calculate the square root of.
+ * @guess: The current guess for the square root.
  *
- * Return: 1 if the strings match, 0 otherwise.
+ * Return: The square root of n if found, -1 if n does not have a natural
+ *         square root.
  */
-int wildcmp(char *s1, char *s2)
+int calculate_sqrt(int n, int guess);
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to calculate the square root of.
+ *
+ * Return: The square root of n if found, -1 if n is negative or does not have
+ *         a natural square root.
+ */
+int _sqrt_recursion(int n)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return 1; /* Base case: both strings are empty */
+	if (n < 0)
+		return (-1); /* Error: Invalid input */
 
-    if (*s1 == *s2 || *s2 == '*')
-        return wildcmp(s1 + 1, s2 + 1); /* Characters match or s2 contains '*' */
+	return (calculate_sqrt(n, 0));
+}
 
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '*')
-            return wildcmp(s1, s2 + 1); /* Skip consecutive '*' characters */
+/**
+ * calculate_sqrt - Calculates the square root of a number using recursion.
+ * @n: The number to calculate the square root of.
+ * @guess: The current guess for the square root.
+ *
+ * Return: The square root of n if found, -1 if n does not have a natural
+ *         square root.
+ */
+int calculate_sqrt(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess); /* Base case: found the square root */
+	else if (guess * guess > n)
+		return (-1); /* Error: n does not have a natural square root */
 
-        if (*(s2 + 1) == '\0')
-            return 1; /* s2 is at the end, s1 can be any remaining characters */
-
-        if (*s1 != '\0')
-            return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2); /* s1 with s2+1 */
-    }
-
-    return 0; /* No match found */
+	return (calculate_sqrt(n, guess + 1)); /* Recursive call */
 }
 
