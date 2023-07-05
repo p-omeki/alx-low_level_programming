7a43fef1052f6977efa48ecece803acedf91523a
p-omeki <partymeki@gmail.com>
2023-07-05 01:16:17 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,32 @@
 #include "main.h"
 
-int wildcmp(char *s1, char *s2);
-
+/**
+ * wildcmp - Compares two strings with wildcard pattern matching.
+ * @s1: The first string.
+ * @s2: The second string with wildcard pattern.
+ *
+ * Return: 1 if the strings match, 0 otherwise.
+ */
 int wildcmp(char *s1, char *s2)
 {
-	/* Base case: if both strings are empty, they are considered identical */
 	if (*s1 == '\0' && *s2 == '\0')
-		return 1;
+		return (1); /* Base case: both strings are empty */
 
-	/* If the current characters match or s2 contains a '*', move to the next characters */
 	if (*s1 == *s2 || *s2 == '*')
-		return wildcmp(s1 + 1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1)); /* Characters match or s2 contains '*' */
 
-	/* If s2 contains a '*', check for matching possibilities by advancing in s2 */
 	if (*s2 == '*')
 	{
-		/* Keep advancing in s2 until a non-* character is found */
 		while (*(s2 + 1) == '*')
-			s2++;
+			s2++; /* Skip consecutive '*' characters */
 
-		/* If s2 is now at the end, s1 can be any remaining characters, so they are considered identical */
 		if (*(s2 + 1) == '\0')
-			return 1;
+			return (1); /* s2 is at the end, s1 can be any remaining characters */
 
-		/* Try matching s1 with s2+1, or s1+1 with s2 if the match fails */
-		return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
+		if (*s1 != '\0')
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)); /* Try matching s1 with s2+1 or s1+1 with s2 */
 	}
 
-	/* If none of the above conditions are met, the strings are not identical */
-	return 0;
+	return (0); /* No match found */
 }
 
