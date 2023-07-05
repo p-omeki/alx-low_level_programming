7c507f58fa45050455ecd14b5aeef2d66cd09193
p-omeki <partymeki@gmail.com>
2023-07-05 01:02:12 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,17 @@
 #include "main.h"
 
 /**
- * is_palindrome_helper - Checks if a string is a palindrome using recursion.
- * @s: The string to check.
- * @start: The starting index.
- * @end: The ending index.
+ * str_length - Calculates the length of a string.
+ * @s: The string.
  *
- * Return: 1 if the string is a palindrome, 0 otherwise.
+ * Return: The length of the string.
  */
-int is_palindrome_helper(char *s, int start, int end);
-
-/**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to check.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
-int is_palindrome(char *s)
+int str_length(char *s)
 {
-	int end = 0;  /* Declare end variable at the top of the function */
+    if (*s == '\0')
+        return 0;
 
-	if (*s == '\0')
-		return (1);  /* Empty string is a palindrome */
-
-	while (s[end] != '\0')
-		end++;
-
-	return (is_palindrome_helper(s, 0, end - 1));
+    return 1 + str_length(s + 1);
 }
 
 /**
@@ -39,12 +24,24 @@ int is_palindrome(char *s)
  */
 int is_palindrome_helper(char *s, int start, int end)
 {
-	if (start >= end)
-		return (1);  /* Palindrome condition met */
+    if (start >= end)
+        return 1;  /* Palindrome condition met */
 
-	if (s[start] != s[end])
-		return (0);  /* Not a palindrome */
+    if (s[start] != s[end])
+        return 0;  /* Not a palindrome */
 
-	return (is_palindrome_helper(s, start + 1, end - 1));
+    return is_palindrome_helper(s, start + 1, end - 1);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to check.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+    int length = str_length(s);
+    return is_palindrome_helper(s, 0, length - 1);
 }
 
