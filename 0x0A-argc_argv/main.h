166beca7ae0c6a6c17b98776f09982572c8b3a02
p-omeki <partymeki@gmail.com>
2023-07-07 11:17:09 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char __attribute__((unused)) *argv[]);
 int main(int argc, char *argv[]);
