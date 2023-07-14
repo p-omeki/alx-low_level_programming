0b1c714a21f9e30144b67fdeaa0095565b1a0fb3
p-omeki <partymeki@gmail.com>
2023-07-14 06:04:54 -0700

    array_range

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 
+int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
