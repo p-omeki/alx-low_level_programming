9c84bb111d0481a0bab55fc4846064b72ce197bc
p-omeki <partymeki@gmail.com>
2023-07-14 05:56:11 -0700

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,18 +14,18 @@ int *array_range(int min, int max)
 	int *arr, size, i;
 
 	if (min > max)
-		return (NULL);
+		return NULL;
 
 	size = max - min + 1;
 
 	arr = malloc(sizeof(int) * size);
 
 	if (arr == NULL)
-		return (NULL);
+		return NULL;
 
 	for (i = 0; i < size; i++)
 		arr[i] = min++;
 
-	return (arr);
+	return arr;
 }
 
