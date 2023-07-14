e950ad41e8f1a383c03a6869f576e765158e8611
p-omeki <partymeki@gmail.com>
2023-07-14 05:25:25 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,16 +12,16 @@ void multiply(char *num1, char *num2);
  */
 int _isdigit(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (0);
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
 
-    return (1);
+	return (1);
 }
 
 /**
@@ -31,46 +31,46 @@ int _isdigit(char *str)
  */
 void multiply(char *num1, char *num2)
 {
-    int len1 = 0, len2 = 0, i, j, k;
-    int *result;
-    int carry = 0;
+	int len1 = 0, len2 = 0, i, j, k;
+	int *result;
+	int carry = 0;
 
-    while (num1[len1] != '\0')
-        len1++;
-    while (num2[len2] != '\0')
-        len2++;
+	while (num1[len1] != '\0')
+		len1++;
+	while (num2[len2] != '\0')
+		len2++;
 
-    result = malloc(sizeof(int) * (len1 + len2));
+	result = malloc(sizeof(int) * (len1 + len2));
 
-    for (i = 0; i < len1 + len2; i++)
-        result[i] = 0;
+	for (i = 0; i < len1 + len2; i++)
+		result[i] = 0;
 
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        carry = 0;
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            k = i + j + 1;
-            result[k] += (num1[i] - '0') * (num2[j] - '0') + carry;
-            carry = result[k] / 10;
-            result[k] %= 10;
-        }
-        result[i] += carry;
-    }
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			k = i + j + 1;
+			result[k] += (num1[i] - '0') * (num2[j] - '0') + carry;
+			carry = result[k] / 10;
+			result[k] %= 10;
+		}
+		result[i] += carry;
+	}
 
-    i = 0;
-    while (i < len1 + len2 && result[i] == 0)
-        i++;
-    if (i == len1 + len2)
-        putchar('0');
-    else
-    {
-        for (; i < len1 + len2; i++)
-            putchar(result[i] + '0');
-    }
+	i = 0;
+	while (i < len1 + len2 && result[i] == 0)
+		i++;
+	if (i == len1 + len2)
+		putchar('0');
+	else
+	{
+		for (; i < len1 + len2; i++)
+			putchar(result[i] + '0');
+	}
 
-    putchar('\n');
-    free(result);
+	putchar('\n');
+	free(result);
 }
 
 /**
@@ -82,19 +82,19 @@ void multiply(char *num1, char *num2)
  */
 int main(int argc, char *argv[])
 {
-    char *num1, *num2;
+	char *num1, *num2;
 
-    if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    num1 = argv[1];
-    num2 = argv[2];
+	num1 = argv[1];
+	num2 = argv[2];
 
-    multiply(num1, num2);
+	multiply(num1, num2);
 
-    return (0);
+	return (0);
 }
 
