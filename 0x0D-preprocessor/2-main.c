5b8db48d0c5c6bf6937852264a3be9f3248323e5
p-omeki <partymeki@gmail.com>
2023-07-17 03:10:23 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 
 int main(void)
 {
-    printf("%s\n", __BASE_FILE__);
+    UNUSED(__BASE_FILE__);
+    printf("%s\n", __FILE__);
     return 0;
 }
 
