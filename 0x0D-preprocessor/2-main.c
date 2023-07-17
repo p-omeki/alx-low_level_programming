9ee0eda831f1319b289b9723f492ac5c35d9672e
p-omeki <partymeki@gmail.com>
2023-07-17 03:15:45 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,20 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the name of the file it was compiled from, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
+
 #define UNUSED(x) (void)(x)
 
 int main(void)
 {
     UNUSED(__BASE_FILE__);
+
     printf("%s\n", __FILE__);
-    return 0;
+
+    return (0);
 }
 
