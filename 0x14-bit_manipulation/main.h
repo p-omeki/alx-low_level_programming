7ff109f3c9fead7a2edd6c5ad92f70da6dbb16d7
p-omeki <partymeki@gmail.com>
2023-08-02 23:45:12 -0700

    0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ *
+ * Return: The converted number, or 0 if there is an invalid char or b is NULL.
+ */
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
+
