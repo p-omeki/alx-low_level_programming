4e02bcf27fa10f3b0a6c527f51088eaeb6481974
p-omeki <partymeki@gmail.com>
2023-08-07 02:29:24 -0700

    Speak gently, she can hear

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,7 @@
 
 /* Function prototype for read_textfile */
 int create_file(const char *filename, char *text_content);
-
+int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* MAIN_H */
