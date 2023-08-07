ab108ee4b14b67aca333e8fd44a919e98d4d10a3
p-omeki <partymeki@gmail.com>
2023-08-07 02:23:51 -0700

    Under the snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 /* Function prototype for read_textfile */
+int create_file(const char *filename, char *text_content);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* MAIN_H */
